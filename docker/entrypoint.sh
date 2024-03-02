590d0fcd8d9def9fb4fe59171820511cbd795fa2
Rúni H.Hansen <runi.hansen@okkara.net>
2024-03-02 20:45:26 +0100

    PostgreSQL v96 only supports psql!

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -3,11 +3,17 @@
 command=$1
 shift
 
+# Postgresql Client v96 only supports psql
+if [ -f /PostgreSQL96.txt ] && [ "$command" != "psql" ];then
+  cat /PostgreSQL96.txt
+  exit 1
+fi
+
 case "$command" in
-  ls|psql|pg_dump|pg_dumpall|pg_basebackup|pg_restore)
+  psql|pg_dump|pg_dumpall|pg_basebackup|pg_restore)
     "$command" "$@"
     exit 0
     ;;
 esac
 echo "I don't know '$command', sorry! =c/"
-exit 0
+exit 1
